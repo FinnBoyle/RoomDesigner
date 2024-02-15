74f68c953fb6d9acf3ddd338d71a0c472a6e19dc
Finn <finnboyle@icloud.com>
2024-02-15 22:26:57 +1300

    button functionality testing

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 import tkinter as tk
 
+buttonSelected = 0
+
+
+def update_selected(value):
+    global buttonSelected
+    buttonSelected = value
+
+
+def test(event):
+    global buttonSelected
+    x = event.x
+    y = event.y
+    if buttonSelected == 1:
+        radius = 75
+        canvas.create_oval(x-radius, y-radius, x+radius, y+radius, outline="red", width=2)
+    elif buttonSelected == 2:
+        radius = 150
+        canvas.create_oval(x-radius, y-radius, x+radius, y+radius, outline="green", width=2)
+    else:
+        l = 50
+        canvas.create_rectangle(x-l, y-l, x+l, y+l, outline="orange", width=4)
+
+
 # Create window
 window = tk.Tk()
 window.title("Room Designer")
@@ -9,21 +32,21 @@ window.minsize(500, 250)
 buttons = tk.Frame(window, bg="lightgreen", borderwidth=7.5, highlightbackground="black", highlightthickness=1)
 buttons.pack(fill=tk.Y, side=tk.LEFT)
 
-buildWalls = tk.Button(master=buttons, text="Build Walls", width=10, bg="lightblue")
+buildWalls = tk.Button(master=buttons, text="Build Walls", width=10, bg="lightblue",
+                       command=lambda: update_selected(1))
 buildWalls.pack()
-addFurnishings = tk.Button(master=buttons, text="Add\nFurnishing", width=10, bg="lightblue")
+addFurnishings = tk.Button(master=buttons, text="Add\nFurnishing", width=10, bg="lightblue",
+                           command=lambda: update_selected(2))
 addFurnishings.pack()
-removeEither = tk.Button(master=buttons, text="Remove\nWalls/\nFurnishing", width=10, bg="lightblue")
+removeEither = tk.Button(master=buttons, text="Remove\nWalls/\nFurnishing", width=10, bg="lightblue",
+                         command=lambda: update_selected(0))
 removeEither.pack()
 
-# Components added here
-workspace = tk.Frame(window, bg="lightyellow")
-workspace.pack(fill=tk.BOTH, expand=True)
 
-# To remove...
-canvas = tk.Canvas(workspace, width=100, height=100, bg="lightyellow")
-canvas.pack()
-canvas.create_oval(25, 25, 75, 75, width=10, fill="black")
+# Components to be added to workspace
+canvas = tk.Canvas(window, width=100, height=100, bg="lightyellow")
+canvas.pack(fill=tk.BOTH, expand=True)
+canvas.bind("<Button-1>", test)
 
 # Build GUI
 window.mainloop()
